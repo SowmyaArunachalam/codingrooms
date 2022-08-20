36a8cc64d42a5f1c0621891b3c6fc434b33e9754
SowmyaArunachalam <106989392+SowmyaArunachalam@users.noreply.github.com>
2022-08-20 14:41:21 +0530

    Add files via upload

---
+++ day_11/Debugging_odd_or_even.py
@@ -0,0 +1,21 @@
+'''
+Read this the code in main.py
+Spot the problems 🐞.
+Modify the code to fix the program.
+Fix the code so that it works and passes the tests when you submit.
+
+Hint
+Review the previous lesson and go through the 10 steps to tackle these debugging problems.
+Test Your Code
+Check your code is doing what it is supposed to. When you're happy with your code, click submit to check your solution.
+
+Solution
+https://repl.it/@appbrewery/day-13-1-solution
+'''
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
